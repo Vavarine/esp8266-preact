4efd06eb918307b47d27d1c8b6395227aed25be4
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2024-08-18 15:30:22 -0300

    feat: frontent inside progmem

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { espViteBuild } from "@ikoz/vite-plugin-preact-esp32";
 import "dotenv/config";
 
 const proxyTarget: string | undefined = process.env.VITE_PROXY;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [espViteBuild(), preact()],
   resolve: {
     alias: {
       "@components": "/src/components",
